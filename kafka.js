8f51b22736b0d3642903b87b1b544c632baa6543
Gareth Murphy <garethcmurphy@gmail.com>
2019-04-08 14:06:27 +0200

    add fiel

--- kafka.js
+++ kafka.js
@@ -120,7 +120,7 @@ async function postToSciCat(token, message, config, sampleId) {
   }
   let fileName = "default.nxs"
   if (scimetObject.hasOwnProperty('file_attributes')) {
-    fileName = dataset.scientificMetadata.file_attributes.file_name;
+    fileName = scimetObject.file_attributes.file_name;
   }
   let newObject = {
     start_time : dateNow,

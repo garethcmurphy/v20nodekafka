8fc5cee87713f05bb389f01c8c7cb14c7f22d5c3
Gareth Murphy <garethcmurphy@gmail.com>
2019-04-09 04:47:33 +0200

    try

--- kafka.js
+++ kafka.js
@@ -25,7 +25,6 @@ var client = new Client({ kafkaHost: dmscip + ":9093" });
 var topics = [{ topic: topic, partition: 0 }];
 var options = {
   kafkaHost: dmscip+":9093",
-  autoCommit: false,
   apiVersionRequest: true,
   fetchMaxWaitMs: 1000,
   fetchMaxBytes: 16 * 1024 * 1024,
@@ -34,19 +33,6 @@ var options = {
 };
 
 var consumer = new ConsumerGroup(option, topics);
-var offset = new Offset(client);
-offset.fetchLatestOffsets([topic], (err, offsets) => {
-  if (err) {
-      console.log(`error fetching latest offsets ${err}`)
-      return
-  }
-  var latest = 1
-  Object.keys(offsets[topic]).forEach( o => {
-    latest = offsets[topic][o] > latest ? offsets[topic][o] : latest;
-    console.log("latest offset", latest);
-  })
-  consumer.setOffset(topic, 0, latest-1)
-})
 
 // Refresh metadata required for the first message to go through
 // https://github.com/SOHU-Co/kafka-node/pull/378

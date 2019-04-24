dad20546d3e155060a8ffcf2887d87cd1ad599b7
Gareth Murphy <garethcmurphy@gmail.com>
2019-04-24 06:06:38 +0200

    Update kafka.ts

--- kafka.ts
+++ kafka.ts
@@ -60,8 +60,8 @@ async function sendtoscicat(message, config) {
   var x = await loginToScicat(config);
   let sampleId = shortid.generate();
   var dataset = await postToSciCat(x, message, config, sampleId);
-  //var z = await sampleToSciCat(x, dataset, config, sampleId);
-  // var q = await origToSciCat(x, dataset, message, config, sampleId);
+  var z = await sampleToSciCat(x, dataset, config, sampleId);
+  var q = await origToSciCat(x, dataset, message, config, sampleId);
 }
 
 consumer.on("message", function (message) {

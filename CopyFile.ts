ac568a0448ba195c36e62bcac2015b91c497800b
Gareth Murphy <garethcmurphy@gmail.com>
2019-05-03 14:15:51 +0200

    add copy function

--- CopyFile.ts
+++ CopyFile.ts
@@ -1,25 +1,46 @@
 "use strict";
+var fs = require("fs");
+var client = require("scp2");
 
-
-var fs = require('fs');
 export class CopyFile {
-    destination: string;
-    constructor() {
-        this.destination = "remote.com";
-        this.readConfig();
-        
-    }
+  destination: string;
+  userName: string;
+  privateKey: string;
+  path: string;
+  constructor() {
+    this.destination = "remote.com";
+    this.userName = "remote.com";
+    this.readConfig();
+  }
 
-    readConfig() {
+  readConfig() {
+    const filename = "configRemote.json";
+    var remoteData = JSON.parse(fs.readFileSync(filename, "utf8"));
+    this.destination = remoteData.remoteHost;
+    this.userName = remoteData.userName;
+    this.path = remoteData.path;
+    this.privateKey = remoteData.privateKey;
+  }
 
-        const filename = "configRemote.json";
-        var remoteData = JSON.parse(fs.readFileSync(filename, "utf8"));
-        this.destination = remoteData.remoteHost;
+  
 
-    }
+  copy() {
+    client.scp(
+      "file2.txt",
+        {
+          host: this.destination,
+            username: this.userName,
+            privateKey: fs.readFileSync(this.privateKey, "utf8"),
+          path: this.path
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+}
 
-
-
-
-
+if (require.main === module) {
+  let copier = new CopyFile();
+  copier.copy();
 }

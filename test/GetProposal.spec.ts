d42ee5e047b28d8dabda0372c6a0dcca1c83e62e
Gareth Murphy <garethcmurphy@gmail.com>
2019-05-17 07:11:29 +0200

    ad dunit test

--- test/GetProposal.spec.ts
+++ test/GetProposal.spec.ts
@@ -1,6 +1,6 @@
 import { GetProposal } from './../src/GetProposal';
 var assert = require('assert');
-describe('Array', function() {
+describe('get Proposal', function() {
     it('should return default proposal string', function() {
       let proposal = new GetProposal();
       let id = proposal.get();

166a46afd57485ae7933d033b19a2fa223f78d08
Gareth Murphy <garethcmurphy@gmail.com>
2019-05-27 14:10:44 +0200

    adding measurement period

--- src/GetProposal.ts
+++ src/GetProposal.ts
@@ -2,15 +2,29 @@
 
 export class GetProposal {
   proposalId = "GH43YU";
+
   proposal = {
     proposalId: "GH43YU",
     MeasurementPeriodList: [
       {
         start: "2019-05-27T11:03:32.595Z",
-        end: "2019-05-27T11:03:32.595Z"
+        end: "2019-05-29T11:03:32.595Z"
       }
     ]
   };
+  proposalArray = [];
+
+    constructor() {
+        this.proposalArray.push(this.proposal);
+  }
+
+  checkIfDateInProposalRange() {
+    let today = new Date(Date.now());
+    for (const prop of this.proposalArray) {
+      let x = 0;
+    }
+    return 0;
+  }
 
   getDate() {
     return new Date(Date.now());

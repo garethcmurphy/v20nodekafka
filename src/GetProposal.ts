0ac161e6cfa54bddf8627358fbe03473a3666606
Gareth Murphy <garethcmurphy@gmail.com>
2019-06-10 09:28:33 +0200

    add query to get rpoposal

--- src/GetProposal.ts
+++ src/GetProposal.ts
@@ -1,21 +1,46 @@
 "use strict";
 
+const rp = require("request-promise");
+
 export class GetProposal {
-    proposalId = "GH43YU";
+  proposalId = "GH43YU";
 
-    getDate(){
-        return new Date(Date.now());
+  getDate() {
+    return new Date(Date.now());
+  }
+
+  async get() {
+    // login to catamel
+    let accessToken = "fhs";
+
+    let base_url = "http://localhost:3000/Proposals/findByInstrumentAndDate?instrument=V20&measureTime=20190101";
+    let url = base_url+"?access_token="+accessToken;
+
+    let options = {
+      url: url,
+      method: "GET",
+      json: true,
+      rejectUnauthorized: false,
+      requestCert: true
+    };
+
+    try {
+      const response = await rp(options);
+      Promise.resolve(response);
+    } catch (error) {
+      console.log(url);
+      console.log(error);
+      return Promise.reject(error);
     }
 
-    get(){
+    // get proposal from catamel
 
-        return this.proposalId;
-    }
+    return this.proposalId;
+  }
 }
 
-
 if (require.main === module) {
-	let read = new GetProposal();
-    let proposalId = read.get();
-    console.log(proposalId);
+  let read = new GetProposal();
+  let proposalId = read.get();
+  console.log(proposalId);
 }
